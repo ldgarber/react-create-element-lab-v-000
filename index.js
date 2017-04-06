dee0d7b72d8c5af845ba449bf80c49f354293e65
ldgarber <leah.garber88@gmail.com>
2017-04-06 16:26:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Code React element here
+var h1 = React.createElement('h1', {}, "An Awesome Person"); 
+var p = React.createElement('p', {}, 'Who is learning React'); 
+var ul = React.createElement('ul', {className: 'me__interests'}, [
+  React.createElement('li', {}, "JavaScript"), 
+  React.createElement('li', {}, "React"), 
+  React.createElement('li', {}, "Movies"), 
+  React.createElement('li', {}, "Ice cream")
+]); 
+var meInReact = React.createElement('div', {className: 'me'}, [h1, p, ul])
+ReactDOM.render(meInReact); 
